1ae3944f49e9ec452d2f6a2884d29b06331773a6
LuckyDogLee <570556057@qq.com>
2016-02-05 00:02:59 +0800

    login page
    login page

---
+++ anonymity-new/js/login.js
@@ -0,0 +1,20 @@
+var hbtn_login = document.getElementById("hbtn-login");
+hbtn_login.onclick = function(){
+	var	mask = document.getElementById("mask");
+	mask.style.visibility = "visible";
+
+	var login_bar = document.getElementById("login-bar");
+	login_bar.style.position = "static";
+	login_bar.style.left = "0";
+}
+
+var btn_close = document.getElementById("btn-close");
+btn_close.onclick = function(){
+	var	mask = document.getElementById("mask");
+	mask.style.visibility = "hidden";
+
+	var login_bar = document.getElementById("login-bar");
+	login_bar.style.position = "absolute";
+	login_bar.style.left = "-999em";
+}
+
