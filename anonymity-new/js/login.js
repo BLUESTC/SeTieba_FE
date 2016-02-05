604b1756939a057bddc97270970edf9779c40257
LuckyDogLee <570556057@qq.com>
2016-02-05 23:19:51 +0800

    。
    。

--- anonymity-new/js/login.js
+++ anonymity-new/js/login.js
@@ -19,5 +19,30 @@ btn_close.onclick = function(){
 	var login_bar = document.getElementById("login-bar");
 	login_bar.style.position = "absolute";
 	login_bar.style.left = "-999em";
+
+	var	login_bar_parent = document.getElementById("login-bar-parent");
+	login_bar_parent.style.cssText = "z-index: -1;";
+}
+
+/* 验证用户账号信息，若正确，则进入管理系统，若错误，输入边框和字体变红色 */
+var btn_login = document.getElementById("btn-login");
+btn_login.onclick = function(){
+  	var email, password;
+  	email = document.getElementById("email").value;
+  	password = document.getElementById("password").value;
+
+  	if (email=="570556057@qq.com" && password=="123456") {
+  		window.location.href = "home.html";
+  	}else{
+  		document.getElementById("email").className += ' invalid';
+  		document.getElementById("password").className += ' invalid';
+  	}
+}
+
+/* 恢复边框的正常样式 */
+function stycleChange()
+{
+	document.getElementById("email").className = 'email item';
+  	document.getElementById("password").className = 'password item';
 }
 
