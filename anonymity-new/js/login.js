0cc32a0d87d51463ff104d288d265bc1f26c8cdf
LuckyDogLee <570556057@qq.com>
2016-02-05 13:08:04 +0800

    register page
    register page

--- anonymity-new/js/login.js
+++ anonymity-new/js/login.js
@@ -6,6 +6,9 @@ hbtn_login.onclick = function(){
 	var login_bar = document.getElementById("login-bar");
 	login_bar.style.position = "static";
 	login_bar.style.left = "0";
+
+	var	login_bar_parent = document.getElementById("login-bar-parent");
+	login_bar_parent.style.cssText = "z-index: 3000;";
 }
 
 var btn_close = document.getElementById("btn-close");
