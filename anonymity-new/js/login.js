3407e392abfc08b289c88860fa5e24c15070ac70
LuckyDogLee <570556057@qq.com>
2016-02-06 22:55:52 +0800

    登录
    登录状态样式的改变

--- anonymity-new/js/login.js
+++ anonymity-new/js/login.js
@@ -1,3 +1,26 @@
+window.onload = function(){
+	//flag表示登录状态，为true时表示已登录
+	var flag = localStorage.getItem("flag");
+	if (flag === "true") {
+		flag = true;
+	}else{
+		flag = false;
+	};
+	if (flag) {
+		var login_register_bar = document.getElementById("login-register-bar");
+  		login_register_bar.style.visibility = "hidden";
+
+  		var user_bar = document.getElementById("user-bar");
+  		user_bar.style.visibility = "visible";
+	}else{
+		var login_register_bar = document.getElementById("login-register-bar");
+  		login_register_bar.style.visibility = "visible";
+
+  		var user_bar = document.getElementById("user-bar");
+  		user_bar.style.visibility = "hidden";
+	};
+}
+
 var hbtn_login = document.getElementById("hbtn-login");
 hbtn_login.onclick = function(){
 	var	mask = document.getElementById("mask");
@@ -12,7 +35,7 @@ hbtn_login.onclick = function(){
 }
 
 var btn_close = document.getElementById("btn-close");
-btn_close.onclick = function(){
+var close_onclick = function(){
 	var	mask = document.getElementById("mask");
 	mask.style.visibility = "hidden";
 
@@ -23,21 +46,39 @@ btn_close.onclick = function(){
 	var	login_bar_parent = document.getElementById("login-bar-parent");
 	login_bar_parent.style.cssText = "z-index: -1;";
 }
+btn_close.onclick = close_onclick;
+
 
 /* 验证用户账号信息，若正确，则进入管理系统，若错误，输入边框和字体变红色 */
 var btn_login = document.getElementById("btn-login");
 btn_login.onclick = function(){
-  	var email, password;
-  	email = document.getElementById("email").value;
-  	password = document.getElementById("password").value;
+  	var email = document.getElementById("email").value;
+  	var password = document.getElementById("password").value;
 
   	if (email=="570556057@qq.com" && password=="123456") {
-  		window.location.href = "home.html";
+  		var user_bar = document.getElementById("user-bar");
+  		user_bar.style.visibility = "visible";
+  		//flag表示登录状态，为true时表示已登录
+  		localStorage.setItem("flag", true);
+  		close_onclick();
   	}else{
   		document.getElementById("email").className += ' invalid';
   		document.getElementById("password").className += ' invalid';
   	}
 }
+
+// 退出当前账号
+var hbtn_lgout = document.getElementById("hbtn-lgout");
+hbtn_lgout.onclick = function(){
+	var login_register_bar = document.getElementById("login-register-bar");
+  	login_register_bar.style.visibility = "visible";
+
+  	var user_bar = document.getElementById("user-bar");
+  	user_bar.style.visibility = "hidden";
+  	//flag表示登录状态，为true时表示已登录
+  	localStorage.setItem("flag", false);
+}
+
 
 /* 恢复边框的正常样式 */
 function stycleChange()
